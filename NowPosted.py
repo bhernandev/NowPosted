913750804379b7c9eeb6e5bcf3b76672af889491
Brian Hernandez <Brian@Hernandez-cs.com>
2016-10-20 19:52:20 -0400

    Credentials and their use now added to NowPosted.py

--- NowPosted.py
+++ NowPosted.py
@@ -1,1 +1,20 @@
-import indeed
+#Importing flask variables for a server side application
+from flask import Flask, request, redirect, session
+
+#Importing the Twilio REST API, and twiml for building Twilio responses
+from twilio.rest import TwilioRestClient
+import twilio.twiml
+
+#Importing the indeed Python module for indeed requrests
+from indeed import IndeedClient
+
+#Importing the credentials for the various APIs and libs used
+import credentials
+
+#For json parsing and creation if we need it
+import json
+
+app = Flask(__name__)
+
+twilio_api = TwilioRestClient(credentials.my_twilio_account_sid, credentials.my_twilio_auth_token)
+indeedd_api = IndeedClient(publisher = credentials.my_indeed_publisher_id)
