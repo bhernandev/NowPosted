c59693dbb97d191790beb3bd0cc4cc8db14447b9
Brian Hernandez <Brian@Hernandez-cs.com>
2016-10-23 23:26:47 -0400

    small parameter bug fixed

--- NowPosted.py
+++ NowPosted.py
@@ -30,14 +30,11 @@ twilio_api = TwilioRestClient(credentials.my_twilio_account_sid, credentials.my_
 indeed_api = IndeedClient(publisher = credentials.my_indeed_publisher_id)
 
 def print_date_time(string, string2):
-    print(string)
-    print(string2)
-
-scheduler = BackgroundScheduler()
-scheduler.start()
-scheduler.add_job(func=print_date_time, trigger=IntervalTrigger(seconds=5), args={'string', 'string2'}, id='printing_job', name='Print date and time every five seconds', replace_existing=True)
-atexit.register(lambda: scheduler.shutdown())
-
+    #first string
+    print("first parameter " + string)
+    #second string
+    print("second parameter " + string2)
+    return None
 
 def FindJobs(user_number):
     # query_param = userInfo['users'][user_number]['query']
@@ -58,6 +55,11 @@ def FindJobs(user_number):
 #FindJobs(user_number)
 #make the request with the parameters from the "database"
 #for every existing job for this phone number, eliminate from the search to have only 10 show up
+
+scheduler = BackgroundScheduler()
+scheduler.start()
+scheduler.add_job(func=print_date_time, trigger=IntervalTrigger(seconds=5), args=['string1','string2'], id='printing_job', name='Print date and time every five seconds', replace_existing=True)
+atexit.register(lambda: scheduler.shutdown())
 
 @app.route("/", methods=['GET', 'POST'])
 def DeliverJobs():
