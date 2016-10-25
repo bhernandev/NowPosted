e03b78638174b9f23f00471e34307264260d64c9
Brian Hernandez <Brian@Hernandez-cs.com>
2016-10-25 19:16:19 -0400

    Now sends all jobs posted within the last day

--- NowPosted.py
+++ NowPosted.py
@@ -38,22 +38,19 @@ def FindAndDeliverJobs():
             'l': "",
             'userip': credentials.my_ip_address, #this might be a little tough to circumvent, maybe pull data from the form when signing up for NowPosted?
             'useragent': credentials.my_user_agent, #use my own computers credentials?
+            'fromage': 1
         }
         indeed_response = indeed_api.search(**params)
-        job_delivery = indeed_response['results'][0]['jobtitle'] + " at " + indeed_response['results'][0]['company']
-        twilio_api.messages.create(to=user['phone_number'], from_=credentials.my_twilio_number, body=job_delivery)
-        print("(" + job_delivery + ") sent to: " user['phone_number'])
+        for job in indeed_response['results']:
+            job_delivery = job['jobtitle'] + " at " + job['company']
+            twilio_api.messages.create(to=user['phone_number'], from_=credentials.my_twilio_number, body=job_delivery)
+            print("(" + job_delivery + ") sent to: " + user['phone_number'])
     #old_jobs = userInfo['users'][user_number]['old_jobs']
     #loop that checks if any of the 50 new results for indeed exist in the old jobs array. if they do, remove from the json response
     #new_results = newest results from indeed search
     #return new_results
 
-#some loop that creates time synced queueing ()
-#for every phone number in clientele array
-#FindJobs(user_number)
-#make the request with the parameters from the "database"
-#for every existing job for this phone number, eliminate from the search to have only 10 show up
-
+#this block of code sets up for doing a function every x seconds
 scheduler = BackgroundScheduler()
 scheduler.start()
 scheduler.add_job(func=FindAndDeliverJobs, trigger=IntervalTrigger(seconds=30), id='printing_job', name='Print date and time every five seconds', replace_existing=True)
@@ -64,7 +61,6 @@ def DefaultRequestHandler():
     global name
     global query
     if request.method == 'GET':
-        #return the website with the signup form
         return app.send_static_file('home.html')
     elif request.method == 'POST':
         name = request.form['phone_number']
@@ -78,11 +74,6 @@ def DefaultRequestHandler():
         with open('user_info.json', "w") as write_file:
             json.dump(user_list, write_file)
 
-        #with open('user_info.json', "w") as json_file:
-            #json_file.write(json.dumps(user_info))
-
-        #get form data from request object and place into a dict
-        #attach to the json file http://stackoverflow.com/questions/23111625/how-to-add-a-key-value-to-json-data-retrieved-from-a-file-with-python
         return app.send_static_file('thankyou.html')
 
 if __name__ == "__main__":
